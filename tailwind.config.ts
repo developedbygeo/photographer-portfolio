785f26b6576749f5ac33ba8ead43f4e9edd79160
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-17 02:12:31 +0100

    Add config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,68 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
-const config: Config = {
+const config = {
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+        '4xl': '1600px',
+        '6xl': '1800px',
+      },
+    },
+    fontSize: {
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '2rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2.25rem' }],
+      '3xl': ['1.75rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2rem', { lineHeight: '2.5rem' }],
+      '5xl': ['2.5rem', { lineHeight: '3rem' }],
+      '6xl': ['3rem', { lineHeight: '3.5rem' }],
+      '7xl': ['4rem', { lineHeight: '4.5rem' }],
+      '8xl': ['6rem', { lineHeight: '4.5rem' }],
+      '9xl': ['8rem', { lineHeight: '4.5rem' }],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['var(--font-montserrat)', ...defaultTheme.fontFamily.sans],
+        display: ['var(--font-libre)', ...defaultTheme.fontFamily.serif],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
-};
+  plugins: [
+    require('tailwindcss-animate'),
+    require('tailwind-scrollbar')({ nocompatible: true }),
+    ({ addVariant }: any) => {
+      addVariant('group-active-link', ':merge(.group).active-link &');
+    },
+  ],
+} satisfies Config;
+
 export default config;
