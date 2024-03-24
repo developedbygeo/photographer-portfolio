cc71b58917c163398a6ed560542c79a413a84946
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-24 23:38:19 +0100

    Add social media data

---
+++ data/socialMedia.tsx
@@ -0,0 +1,21 @@
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+
+const socialMedia = [
+  {
+    name: 'Facebook',
+    href: 'https://www.facebook.com',
+    icon: FaFacebook,
+  },
+  {
+    name: 'Instagram',
+    href: 'https://www.instagram.com',
+    icon: FaInstagram,
+  },
+  {
+    name: 'Twitter',
+    href: 'https://twitter.com',
+    icon: FaTwitter,
+  },
+];
+
+export default socialMedia;
