04a8230354f64198ed353f2676d1d5c3b4e2b8ae
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-26 23:43:44 +0100

    Refactor types in data.ts

--- types/data.ts
+++ types/data.ts
@@ -1,19 +1,32 @@
-export type CmsImage = {
-    id: string;
-    src: string;
-    alt: string;
-    text: string;
-    isDark: boolean;
+// export type CmsImage = {
+//     id: string;
+//     src: string;
+//     alt: string;
+//     text: string;
+//     isDark: boolean;
+// };
+
+// export type HeroImageTextCard = {
+//     title: string;
+//     subtitle: string;
+//     description: string;
+//     link: string;
+// };
+
+// export type HeroGalleryImage = {
+//     image: CmsImage;
+//     textCard: HeroImageTextCard;
+// };
+
+export type ImageTextInfo = {
+  title: string;
+  location: string;
+  description: string;
 };
 
-export type HeroImageTextCard = {
-    title: string;
-    subtitle: string;
-    description: string;
-    link: string;
-};
-
-export type HeroGalleryImage = {
-    image: CmsImage;
-    textCard: HeroImageTextCard;
+export type ImageTechnicalInfo = {
+  'Camera Model': string;
+  'Lens Used': string;
+  Aperture: string;
+  'Shutter Speed': string;
 };
